5129ff80024e8d6f3555b9196b860b8422090bda
HotDamnCoder <marcusb2kl@gmail.com>
2019-11-06 20:27:37 +0200

    First commit

---
+++ Audio.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pyaudio
+import wave
+import matplotlib.pyplot as plt
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 15
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+spe = p.get_device_info_by_index(2)
+stream = p.open(format=FORMAT,
+                channels=2,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK,
+                input_device_index = 2)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+
+    data = stream.read(CHUNK)
+    inta = np.frombuffer(data, dtype=np.int16)
+    frames.append(inta)
+stream.stop_stream()
+stream.close()
+stream.stop_stream()
+stream.close()
+p.terminate()
+print("* done recording")
+plt.plot(frames)
+plt.show()
+plt.close()
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+plt.plot(np.fromfile("C:\\Users\\Marcus\\Desktop\\Audio\\output.wav", dtype=np.int16))
+plt.show()
+print ( "done")
